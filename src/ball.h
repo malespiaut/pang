e2c19bd34ef6110617cd62727baf5f5af8542ae2
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-08-29 15:20:00 +0200

    Reviewing ball.h

--- src/ball.h
+++ src/ball.h
@@ -1,8 +1,10 @@
+#pragma once
+
 void showBall(int i);
 int ballCollideWithPlatform(int sprite1, int sprite2, int b);
-void initBalls();
+void initBalls(void);
 void explodeABall(int a);
-void explodeAllBall();
+void explodeAllBall(void);
 int createBigBall(double posx, double posy, int hdirection, int vdirection);
 int createNormalBall(double posx, double posy, int hdirection, int vdirection);
 int createSmallBall(double posx, double posy, int hdirection, int vdirection);
