5c9f25626816356ff4f18445e0cd1bfbd68ff390
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-08-29 15:19:36 +0200

    Reviewing sound.h

--- src/sound.h
+++ src/sound.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL.h>
+
 void initSoundEngine(void);
 void loadSound(char* filenameDreamcast, char* filenamePC, int soundNumber);
 void playSound(int soundNumber, int number);
