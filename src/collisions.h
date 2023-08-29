4be2c45d6bb4499455b4d6cb098d5409d9150c4e
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-08-29 15:19:18 +0200

    Reviewing collisions.h

--- src/collisions.h
+++ src/collisions.h
@@ -1,2 +1,4 @@
+#pragma once
+
 int checkBallCollisionWithBorder(int i);
 int isCollide(int XRectangle1, int YRectangle1, int HRectangle1, int LRectangle1, int XRectangle2, int YRectangle2, int HRectangle2, int LRectangle2);
