88a25fc824fb5e92ad5bb38e41680517f381b32e
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-12-18 03:02:57 +0100

    Reformating events code.

--- src/events.h
+++ src/events.h
@@ -1,34 +1,32 @@
 #pragma once
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include <SDL.h>
 
 enum Key
 {
-	kKey_U,
-	kKey_D,
-	kKey_L,
-	kKey_R,
-	kKey_Confirm,
-	kKey_NUM_KEYS,
+  kKey_U,
+  kKey_D,
+  kKey_L,
+  kKey_R,
+  kKey_Confirm,
+  kKey_NUM_KEYS,
 };
 typedef enum Key Key;
 
-
 enum KeyState
 {
-	kKeyState_Off		= 0b00,
-	kKeyState_Up		= 0b01,
-	kKeyState_Pressed	= 0b10,
-	kKeyState_Held		= 0b11,
-	kKeyState_ActiveBit	= 0b10,
+  kKeyState_Off = 0b00,
+  kKeyState_Up = 0b01,
+  kKeyState_Pressed = 0b10,
+  kKeyState_Held = 0b11,
+  kKeyState_ActiveBit = 0b10,
 };
 typedef enum KeyState KeyState;
 
-extern uint8_t gKeyStates[kKey_NUM_KEYS];
-
+extern KeyState gKeyStates[kKey_NUM_KEYS];
 
 void events_process(void);
-bool GetNewKey(int key);
+bool GetNewKey(Key key);
